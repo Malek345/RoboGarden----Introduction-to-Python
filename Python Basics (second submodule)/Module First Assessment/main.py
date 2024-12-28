e6ab306588afe7226f0efd3d73d489a480823368
Malek Abdelrhman <1901104@eng.asu.edu.eg>
2024-12-28 18:46:34 +0200

    First assessment in the second submodule

---
+++ Python Basics (second submodule)/Module First Assessment/main.py
@@ -0,0 +1,4 @@
+start_year = int(input("Enter the year the person was born or the item was created: "))
+end_year = int(input("Enter the current year: "))
+age = end_year - start_year
+print("The age of the person/item is: " + str(age) + " years")
