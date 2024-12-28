a957a3b274e3c10a432025433ea7fb9a7de2b5ca
Malek Abdelrhman <1901104@eng.asu.edu.eg>
2024-12-28 17:53:22 +0200

    My first module assessment

---
+++ Introduction to Python (first submodule)/Module Assessments/First module assessment/first.py
@@ -0,0 +1,6 @@
+"""
+Prints an introduction with name, age, and project goals.
+"""
+print("Hello! My name is Malek.")
+print("I am 23 years old.")
+print("Through this project, I aim to improve my skills in Python to enter the AI field.")
